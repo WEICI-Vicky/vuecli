57497182b664ec41a33ccaa2fee080be0cbf24c7
Vicky <vicky14714719@gmail.com>
2024-03-13 11:59:33 +0800

    add dist folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,10 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'semi': 0,
+    'space-before-function-paren': 0,
+    'vue/multi-word-component-names': 'off',
+    'comma-dangle': 0
   }
 }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ProfileCard></ProfileCard>
   </div>
 </template>
+
+<script>
+import ProfileCard from '@/components/Card.vue'
+
+export default {
+  components: {
+    ProfileCard
+  }
+}
+</script>
